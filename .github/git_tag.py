b4cd3b6f4c04882a7e1d2c045df90e1e825a93e6
Ching Him Leung <himhim2001@live.hk>
2022-03-02 18:11:56 -0600

    test v2

--- .github/git_tag.py
+++ .github/git_tag.py
@@ -3,16 +3,17 @@ import sys
 import subprocess
 
 if __name__== "__main__":
-    print "test"
+    print ("test")
     """
     current_tag = ""
 
     if len(sys.argv)>1:
         current_tag = sys.argv[1]
-
+    """
     result = subprocess.run(['git', 'tag', '-l', '--sort=committerdate'], stdout=subprocess.PIPE)
     tags_list = (result.stdout).decode("utf-8").split('\n')[:-1]
-
+    print(tags_list)
+    """
     previous_tag="empty"
     flag=False
     for i in reversed(tags_list):
