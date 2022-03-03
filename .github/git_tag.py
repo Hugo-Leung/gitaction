c1e220ec5c094b7d1c7080be7a1c9c134e4dac74
Ching Him Leung <himhim2001@live.hk>
2022-03-02 18:46:58 -0600

    retrive last tag

--- .github/git_tag.py
+++ .github/git_tag.py
@@ -8,17 +8,13 @@ if __name__== "__main__":
     if len(sys.argv)>1:
         current_tag = sys.argv[1]
     result = subprocess.run(['git', 'tag' ,'-l', '--sort=committerdate'], stdout=subprocess.PIPE) 
-    tags_list = (result.stdout).decode("utf-8")#.split('\n')
-    print(tags_list)
-    """
+    tags_list = (result.stdout).decode("utf-8").split('\n')[:-1]
     previous_tag="empty"
     flag=False
     for i in reversed(tags_list):
-        t=i.split()[1]
         if flag or current_tag!=i:
             previous_tag=i
             break
         if current_tag!=i:
             flag=True
     print(previous_tag)
-    """
