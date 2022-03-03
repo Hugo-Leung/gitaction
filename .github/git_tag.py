020716c22574d45af9d3ca9ff95d398c74d25556
Ching Him Leung <himhim2001@live.hk>
2022-03-02 18:36:46 -0600

    ls-remote

--- .github/git_tag.py
+++ .github/git_tag.py
@@ -3,20 +3,18 @@ import sys
 import subprocess
 
 if __name__== "__main__":
-    print ("test")
-    """
     current_tag = ""
 
     if len(sys.argv)>1:
         current_tag = sys.argv[1]
-    """
-    result = subprocess.run(['git', 'tag', '-l', '--sort=committerdate'], stdout=subprocess.PIPE)
+    result = subprocess.run(['git', 'ls-remote', '--tags' ,'-q', '--sort=committerdate'], stdout=subprocess.PIPE) #git 'ls-remote' --tags -q --sort=committerdate
     tags_list = (result.stdout).decode("utf-8").split('\n')[:-1]
-    #print(tags_list)
+    print(tags_list[0].split())
     """
     previous_tag="empty"
     flag=False
     for i in reversed(tags_list):
+        t=i.split()[1]
         if flag or current_tag!=i:
             previous_tag=i
             break
