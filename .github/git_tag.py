2de259fa8079b8f17dc19a31c8e589683e6c9101
Ching Him Leung <himhim2001@live.hk>
2022-03-02 18:09:07 -0600

    test retrive output from python

--- .github/git_tag.py
+++ .github/git_tag.py
@@ -1,20 +1,25 @@
+#/usr/bin/python3
 import sys
 import subprocess
 
-current_tag = ""
+if __name__== "__main__":
+    print "test"
+    """
+    current_tag = ""
 
-if len(sys.argv)>1:
-    current_tag = sys.argv[1]
+    if len(sys.argv)>1:
+        current_tag = sys.argv[1]
 
-result = subprocess.run(['git', 'tag', '-l', '--sort=committerdate'], stdout=subprocess.PIPE)
-tags_list = (result.stdout).decode("utf-8").split('\n')[:-1]
+    result = subprocess.run(['git', 'tag', '-l', '--sort=committerdate'], stdout=subprocess.PIPE)
+    tags_list = (result.stdout).decode("utf-8").split('\n')[:-1]
 
-previous_tag=""
-flag=False
-for i in reversed(tags_list):
-    if flag or current_tag!=i:
-        previous_tag=i
-        break
-    if current_tag!=i:
-        flag=True
-print(previous_tag)
+    previous_tag="empty"
+    flag=False
+    for i in reversed(tags_list):
+        if flag or current_tag!=i:
+            previous_tag=i
+            break
+        if current_tag!=i:
+            flag=True
+    print(previous_tag)
+    """
