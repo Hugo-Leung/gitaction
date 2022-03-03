f0eebe8b4db0735018d645bd7c4aa96fac7c6317
Ching Him Leung <himhim2001@live.hk>
2022-03-02 19:20:34 -0600

    test compile

--- .github/compile.sh
+++ .github/compile.sh
@@ -1,3 +1,13 @@
-prev_tag=$0
+prev_tag=$1
 
-latexmk -pdf thesis
+MAIN="thesis"
+latex_option="-pdf"
+latexmk ${latex_option} -outdir=build ${MAIN}
+
+if [ -n "${prev_tag}" ]; then
+	latexdiff-git -r ${prev_tag} ${MAIN}.tex  --flatten
+	diffName=${MAIN}-diff${prev_tag}
+	echo ${diffName}
+	latexmk ${latex_option} -outdir=diff ${diffName}
+	mv diff/${diffName}.pdf diff/diff.pdf
+fi
